ddeeff572556592b1f01a838ad5486b712b6dbed
ninthshark <kiattisak.dujda@gmail.com>
2017-10-01 22:04:44 +0100

    add referenceIntakes function

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,6 +1,10 @@
 
-var calculator = function(ingredients, quantity) {
-    var totalFibre = 0 , totalSugars = 0, totalEnergyKJ = 0, totalEnergyKCal =0, totalCarbohydrate =0, totalSaturatedFat =0, totalFat=0, totalProtein=0;
+var nutriCal = function(ingredients, quantity, devisor = 100) {
+    var totalFibre = 0 , totalSugars = 0, totalEnergyKJ = 0, totalEnergyKCal =0, totalCarbohydrate =0, totalSaturatedFat =0, totalFat=0, totalProtein=0, totalSalt=0;
+
+    if (ingredients.length === 1) {
+        quantity = quantity.split();
+    }
 
     for (var i=0; i<ingredients.length; i++) {
         if ( ingredients[i].fibre === 'N') {
@@ -8,7 +12,7 @@ var calculator = function(ingredients, quantity) {
         } else if ( ingredients[i].fibre === 'Tr') {
             ingredients[i].fibre = 0.5
         } else {
-            totalFibre += (parseFloat(ingredients[i].fibre)  * parseFloat(quantity[i]))/100;
+            totalFibre += (parseFloat(ingredients[i].fibre)  * parseFloat(quantity[i]))/devisor;
         }
 
         if ( ingredients[i].sugars === 'N') {
@@ -16,7 +20,7 @@ var calculator = function(ingredients, quantity) {
         } else if ( ingredients[i].sugars === 'Tr') {
             ingredients[i].sugars = 0.5
         } else {
-            totalSugars += (parseFloat(ingredients[i].sugars)  * parseFloat(quantity[i]))/100;
+            totalSugars += (parseFloat(ingredients[i].sugars)  * parseFloat(quantity[i]))/devisor;
         }
 
         if ( ingredients[i].energy_kj === 'N') {
@@ -24,7 +28,7 @@ var calculator = function(ingredients, quantity) {
         } else if ( ingredients[i].energy_kj === 'Tr') {
             ingredients[i].energy_kj = 0.5
         } else {
-            totalEnergyKJ += (parseFloat(ingredients[i].energy_kj)  * parseFloat(quantity[i]))/100;
+            totalEnergyKJ += (parseFloat(ingredients[i].energy_kj)  * parseFloat(quantity[i]))/devisor;
         }
 
         if ( ingredients[i].energy_kcal === 'N') {
@@ -32,7 +36,7 @@ var calculator = function(ingredients, quantity) {
         } else if ( ingredients[i].energy_kcal === 'Tr') {
             ingredients[i].energy_kcal = 0.5
         } else {
-            totalEnergyKCal += (parseFloat(ingredients[i].energy_kcal)  * parseFloat(quantity[i]))/100;
+            totalEnergyKCal += (parseFloat(ingredients[i].energy_kcal)  * parseFloat(quantity[i]))/devisor;
         }
 
         if ( ingredients[i].carbohydrate === 'N') {
@@ -40,7 +44,7 @@ var calculator = function(ingredients, quantity) {
         } else if ( ingredients[i].carbohydrate === 'Tr') {
             ingredients[i].carbohydrate = 0.5
         } else {
-            totalCarbohydrate += (parseFloat(ingredients[i].carbohydrate)  * parseFloat(quantity[i]))/100;
+            totalCarbohydrate += (parseFloat(ingredients[i].carbohydrate)  * parseFloat(quantity[i]))/devisor;
         }
 
         if ( ingredients[i].saturated_fat === 'N') {
@@ -48,7 +52,7 @@ var calculator = function(ingredients, quantity) {
         } else if ( ingredients[i].saturated_fat === 'Tr') {
             ingredients[i].saturated_fat = 0.5
         } else {
-            totalSaturatedFat += (parseFloat(ingredients[i].saturated_fat)  * parseFloat(quantity[i]))/100;
+            totalSaturatedFat += (parseFloat(ingredients[i].saturated_fat)  * parseFloat(quantity[i]))/devisor;
         }
 
         if ( ingredients[i].fat === 'N') {
@@ -56,7 +60,7 @@ var calculator = function(ingredients, quantity) {
         } else if ( ingredients[i].fat === 'Tr') {
             ingredients[i].fat = 0.5
         } else {
-            totalFat += (parseFloat(ingredients[i].fat)  * parseFloat(quantity[i]))/100;
+            totalFat += (parseFloat(ingredients[i].fat)  * parseFloat(quantity[i]))/devisor;
         }
 
         if ( ingredients[i].protein === 'N') {
@@ -64,10 +68,16 @@ var calculator = function(ingredients, quantity) {
         } else if ( ingredients[i].protein === 'Tr') {
             ingredients[i].protein = 0.5
         } else {
-            totalProtein += (parseFloat(ingredients[i].protein)  * parseFloat(quantity[i]))/100;
+            totalProtein += (parseFloat(ingredients[i].protein)  * parseFloat(quantity[i]))/devisor;
         }
     }
-    return {energyKCal: totalEnergyKCal, energyKJ: totalEnergyKJ, protein: totalProtein, carbohydrate: totalCarbohydrate, sugars: totalSugars, fat: totalFat, saturatedFat: totalSaturatedFat, fibre: totalFibre}
+    return {energyKCal: totalEnergyKCal, energyKJ: totalEnergyKJ, protein: totalProtein, carbohydrate: totalCarbohydrate, sugars: totalSugars, fat: totalFat, saturatedFat: totalSaturatedFat, fibre: totalFibre, totalSalt}
 }
 
-exports.calculator = calculator;
+var referenceIntakesCal = function(ekcal, protein, sat, carb, fat, salt, sugars) {
+    var [riKcal, riProtein, riSat, riCarb, riFat, riSalt, riSugars] = [ekcal, protein, sat, carb, fat, salt, sugars].map((i) => i * 0.05);
+    
+    return {riKcal, riProtein, riSat, riCarb, riFat, riSalt, riSugars};
+};
+
+module.exports = { nutriCal, referenceIntakesCal };
